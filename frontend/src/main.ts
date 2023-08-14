6ae1e736feeaf7be774b575c437e5e5aef18f0ea
Veress Gergő <VeressG@rri.police.hu>
2023-08-14 15:55:02 +0200

    the style is 80% and added a minimum frontend script

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,5 +3,29 @@ import http from "axios";
 import { z } from "zod";
 
 
+const buyerName = document.getElementById("inpname")! as HTMLInputElement
+const buyerZipCode = document.getElementById("inpzip")! as HTMLInputElement
+const buyerCity = document.getElementById("inpcity")! as HTMLInputElement
+const buyerStreet = document.getElementById("inpstreet")! as HTMLInputElement
+const buyerHouseNumber = document.getElementById("inphouse")! as HTMLInputElement
+const buyerEmail = document.getElementById("inpemail")! as HTMLInputElement
+const buyerPhoneNumber = document.getElementById("inpphone")! as HTMLInputElement
+
+const pizzaOneCount = document.getElementById("amountone")! as HTMLInputElement
+const pizzaTwoCode = document.getElementById("amounttwo")! as HTMLInputElement
+const pizzaThreeCount = document.getElementById("amountthree")! as HTMLInputElement
+const pizzaFourCount = document.getElementById("amountfour")! as HTMLInputElement
+const pizzaFiveCount = document.getElementById("amountfive")! as HTMLInputElement
+const pizzaSixCount = document.getElementById("amountsix")! as HTMLInputElement
+const pizzaSevenCount = document.getElementById("amountseven")! as HTMLInputElement
 
 
+document.getElementById("regist")!.addEventListener("click", () => {
+    const orderCheck = document.getElementById("ordercheck")!
+    orderCheck.removeAttribute("style")
+
+    document.getElementById("outname")!.textContent = buyerName.value
+    document.getElementById("outaddress")!.textContent = buyerZipCode.value + " " + buyerCity.value + " " + buyerStreet.value + " " + buyerHouseNumber.value
+    document.getElementById("outemail")!.textContent = buyerEmail.value
+    document.getElementById("outphone")!.textContent = buyerPhoneNumber.value
+})
